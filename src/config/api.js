48cc72b8dcbac8e9eb8b14da06000ada1c1e055a
jowenzero <jowenzero@gmail.com>
2020-04-28 21:58:37 +0700

    deploy to netlify

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // set config defaults while creating instance
 export const API = axios.create({
-    baseURL: "http://dw15gz2dp-hallo-corona.herokuapp.com/api/v1",
+    baseURL: "https://dw15gz2dp-hallo-corona.herokuapp.com/api/v1",
 });
 
 // alter config after instance is set
