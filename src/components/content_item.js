108b8eed9960950f74576bb1d7ebd2781f3c4b74
jowenzero <jowenzero@gmail.com>
2020-05-01 20:28:58 +0700

    article detail fix

--- src/components/content_item.js
+++ src/components/content_item.js
@@ -9,10 +9,7 @@ const ContentItem = ({item}) => {
         <div className="content-item">
             <img src={ process.env.PUBLIC_URL + `../images/Thumbnail${rand}.png` } alt="" className="content-image"></img>
             <p className="content-info-1">{item.title}</p>
-            <p className="content-info-2">Lorem Ipsum is simply dummy text of the printing
-and typesetting industry. Lorem Ipsum has been the industry's
-standard dummy text ever since the 1500s, when an unknown
-printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p className="content-info-2">{item.description}</p>
         </div>
     )
 }
